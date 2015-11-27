2d8c746804aa05041d16024fb8f7e8469d2cc0e7
HoffsMH <hecker.matthew@gmail.com>
2015-11-27 11:57:41 -0600

    finished javascripting-array-filtering

---
+++ javascripting/array-filtering.js
@@ -0,0 +1,10 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var filtered;
+
+function  evenNumbers (number) {
+  return (number % 2 == 0);
+}
+
+filtered = numbers.filter(function (number) {return evenNumbers(number) });
+
+console.log(filtered);
